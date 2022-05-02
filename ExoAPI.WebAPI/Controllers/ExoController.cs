4533b045237ab48b5408d518880e612231d51e10
RicardoCL <ricunhaster@gmail.com>
2022-05-02 15:09:02 -0300

    2nd commit

--- ExoAPI.WebAPI/Controllers/ExoController.cs
+++ ExoAPI.WebAPI/Controllers/ExoController.cs
@@ -1,4 +1,5 @@
-﻿using ExoAPI.WebAPI.Repositories;
+﻿using ExoAPI.WebAPI.Models;
+using ExoAPI.WebAPI.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,7 +8,7 @@ namespace ExoAPI.WebAPI.Controllers
     [Produces("application/json")]
 
     [Route("api/[controller]")]
-    
+
     [ApiController]
     public class ExoController : ControllerBase
     {
@@ -17,7 +18,7 @@ namespace ExoAPI.WebAPI.Controllers
             _exoRepository = exoRepository;
         }
 
-    
+
         [HttpGet]
 
         public IActionResult Listar()
@@ -34,6 +35,87 @@ namespace ExoAPI.WebAPI.Controllers
             }
 
         }
-    }
 
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                ExoProjeto projetoBuscado = _exoRepository.BuscarPorId(id);
+
+                if (projetoBuscado == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(projetoBuscado);
+            }
+
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        //[Authorize(Roles = "1")]
+
+        [HttpPost]
+
+        public IActionResult Cadastrar(ExoProjeto projeto)
+        {
+            try
+            {
+                _exoRepository.Cadastrar(projeto);
+
+                return StatusCode(201);
+
+            }
+
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Cadastrar(int id, ExoProjeto projeto)
+        {
+
+            try
+            {
+                _exoRepository.Atualizar(id, projeto);
+
+                return StatusCode(204);
+
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        //[Authorize(Roles = "1")
+        [HttpDelete("{id}")]
+
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _exoRepository.Deletar(id);
+
+                return Ok("Projeto deletado");
+
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+    }
 }
--- ExoAPI.WebAPI/Repositories/ExoRepository.cs
+++ ExoAPI.WebAPI/Repositories/ExoRepository.cs
@@ -16,6 +16,50 @@ namespace ExoAPI.WebAPI.Repositories
         {
             return _context.Projects.ToList();
         }
+
+        public ExoProjeto BuscarPorId(int id)
+        {
+            return _context.Projects.Find(id);
+        }
+
+        public void Cadastrar(ExoProjeto projeto)
+        {
+            _context.Add(projeto);
+
+            _context.SaveChanges();
+        }
+
+        public void Atualizar(int id, ExoProjeto projeto)
+        {
+            ExoProjeto projetoBuscado = _context.Projects.Find(id);
+
+            if (projetoBuscado != null)
+            {
+                projetoBuscado.Titulo =projeto.Titulo;
+                projetoBuscado.Status = projeto.Status;
+                projetoBuscado.Requisitos = projeto.Requisitos;
+                projetoBuscado.DataInicio = projeto.DataInicio;
+            }
+
+            _context.Projects.Update(projetoBuscado);
+
+            _context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            ExoProjeto projetoBuscado = _context.Projects.Find(id);
+
+            _context.Projects.Remove(projetoBuscado);
+
+            _context.SaveChanges();
+
+
+        }
+
+
     }
+
+
 
 }
